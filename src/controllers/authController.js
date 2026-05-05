59e9781aa230963ca02654ef1c468360bf7f158c
DevNiloy <anisul2917@gmail.com>
2026-05-06 02:25:37 +0600

    changed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,7 @@ const setTokenCookie = (res, token) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    sameSite: "strict",
+    sameSite: "none",
   };
   res.cookie("jwt", token, cookieOptions);
 };
