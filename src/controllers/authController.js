4bb9f0a54fbf2681e535ee78d453e95aa60dd166
DevNiloy <anisul2917@gmail.com>
2026-05-06 03:25:29 +0600

    changed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,8 @@ const {
 const setTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
+    secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     sameSite: "none",
   };
